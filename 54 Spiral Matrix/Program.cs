9adbd2a2a079b8de8eaefb474e28a9a853a96db1
Dmitry <dmitry@instigatemobile.com>
2022-04-11 16:10:04 +0400

    54

---
+++ 54 Spiral Matrix/Program.cs
@@ -0,0 +1,1 @@
+﻿new Solution().SpiralOrder(new int[][] { new int[] { 1, 2, 3, 4 }, new int[] { 5,6,7,8 }, new int[] { 9, 10, 11, 12 } });
---
+++ 54 Spiral Matrix/Solution.cs
@@ -0,0 +1,52 @@
+﻿public class Solution
+{
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        var result = new List<int>();
+        var colBegin = 0;
+        var rowBegin = 0;
+        var rowEnd = matrix.Length - 1;
+        var colEnd = matrix[0].Length - 1;
+
+        while (rowBegin <= rowEnd && colBegin <= colEnd)
+        {
+            // Traverse right
+            for (int _ = colBegin; _ <= colEnd; _++)
+            {
+                result.Add(matrix[rowBegin][_]);
+            }
+            rowBegin++;
+
+            // traverse down
+            for (int _ = rowBegin; _ <= rowEnd; _++)
+            {
+                result.Add(matrix[_][colEnd]);
+            }
+            colEnd--;
+
+            // traverse left
+            if (rowBegin <= rowEnd)
+            {
+                for (int _ = colEnd; _ >= colBegin; _--)
+                {
+                    result.Add(matrix[rowEnd][_]);
+                }
+                rowEnd--;
+            }
+
+            //traverse up
+            if (colBegin <= colEnd)
+            {
+                for (int _ = rowEnd; _ >= rowBegin; _--)
+                {
+                    result.Add(matrix[_][colBegin]);
+                }
+                colBegin++;
+            }
+
+        } 
+
+        return result;
+    }
+
+}
