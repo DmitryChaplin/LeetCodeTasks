14b57fab545b9fc257c05594e6d1de1fedbaaa9f
Dmitry <dmitry@instigatemobile.com>
2022-04-12 16:07:33 +0400

    74

---
+++ 74 Search a 2D Matrix/Program.cs
@@ -0,0 +1,2 @@
+﻿//new Solution().SearchMatrix(new int[][] { new int[] { 1, 3, 5, 7 }, new int[] { 10, 11, 16, 20 }, new int[] { 23, 30, 34, 60 } }, 3);
+new Solution().SearchMatrix(new int[][] { new int[] { 1,1 } }, 2);
---
+++ 74 Search a 2D Matrix/Solution.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        var targetFound = false;
+        var row = 0;
+        var col = matrix[0].Length - 1;
+
+        while ((row < matrix.Length && col >= 0) || targetFound)
+        {
+            if (matrix[row][col] == target)
+            {
+                targetFound = true;
+                break;
+            }
+
+            if (matrix[row][col] > target)
+                col--;
+            else
+                row++;
+        }
+
+        return targetFound;
+    }
+}
