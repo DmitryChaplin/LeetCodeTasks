5423b10154ab49a1c94ddf9836fed8b151dbba37
Dmitry <dmitry@instigatemobile.com>
2022-04-12 11:41:43 +0400

    73, fix

--- 73 Set Matrix Zeroes/Solution.cs
+++ 73 Set Matrix Zeroes/Solution.cs
@@ -2,41 +2,42 @@
 {
     public void SetZeroes(int[][] matrix)
     {
-       // iterate through the second row..n and flag everything
-       for(int y = 1; y < matrix.Length; y++)
-       {
-            for (int x = 0; x < matrix[y].Length; x++)
-            {
+        bool wipeTheFirstCol = false;
+        int rowsNum = matrix.Length;
+        int cellsNum = matrix[0].Length;
+       
+        // iterate through the second row..n and flag everything
+        for (int row = 0; row < rowsNum ; row++)
+        {
+            // if we have any 0's in the first row/first cell of column, we need to set the flag to 0 the first row
+            if (matrix[row][0] == 0)
+                wipeTheFirstCol = true;
+
+            for (int col = 1; col < cellsNum; col++)
                 // check if current cell value is 0
-                if (matrix[y][x] == 0)
+                if (matrix[row][col] == 0)
                 {
-                    // flag row for 0
-                    matrix[y][0] = 0;
-                    // flag col for 0
-                    matrix[0][x] = 0;
-                }
-            }
-       }
+                    matrix[row][0] = 0; // flag row for 0
+                    matrix[0][col] = 0; // flag col for 0
+                };            
+        }
 
         // let's iterate again, checking the flags, excluding the first row and the first column, since we used them for flags
-        for (int y = 1; y < matrix.Length; y++)
-        {
-            for (int x = 1; x < matrix[y].Length; x++)
-            {
+        for (int row = 1; row < rowsNum; row++)
+            for (int cell = 1; cell < cellsNum; cell++)
                 // check if row / col is flagged for 0
-                if (matrix[y][0] == 0 || matrix[0][x] == 0)
-                    matrix[y][x] = 0;
-            }
-        }
+                if (matrix[row][0] == 0 || matrix[0][cell] == 0)
+                    matrix[row][cell] = 0;
 
-        // check if we need to 0 the first row / column
+        // check if we need to 0 the first row, checking the first cell in the given matrix
         if (matrix[0][0] == 0)
-        {
-            for (int i = 0; i < matrix[0].Length; i++)
-                matrix[0][i] = 0;
-            for (int i = 0; i < matrix.Length; i++)
-                matrix[i][0] = 0;
-        }
+            for (int cell = 0; cell < cellsNum; cell++)
+                matrix[0][cell] = 0;
 
+        // we might still have 'leftover' 0's in the first row that we need to act upon
+        // check if we need to 0 the first column
+        if (wipeTheFirstCol)
+            for (int row = 0; row < rowsNum; row++)
+                matrix[row][0] = 0;
     }
 }
