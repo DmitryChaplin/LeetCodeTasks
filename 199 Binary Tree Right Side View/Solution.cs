f54c64df501f1a77b93e2c5f3dac5a7bec468d96
Dmitry <dmitry@instigatemobile.com>
2022-04-14 12:56:08 +0400

    199. not yet finished

---
+++ 199 Binary Tree Right Side View/Solution.cs
@@ -0,0 +1,37 @@
+﻿
+public class Solution
+{
+    public IList<int> RightSideView(TreeNode root)
+    {
+        // haven't finished this task yet, new internal project came up
+        // the idea is to traverse tree from the right side first, checking the current level/count, 
+        // with endlist priority looking like this :
+        // rightSubtree[rightNode], rightSubtree[leftNode], leftsubtree[rightNode], leftSubtree[leftNode]
+        // if we already have an item in a list at a current level/index, skip
+        var list = new List<int>();
+        TraverseRight(root, list, 0);
+
+        return list;
+    }
+
+    void TraverseRight(TreeNode node, List<int> list, int level)
+    {
+        if (node != null)
+        {
+            // we can only have 1 value selected at any given 'level, hence let's check if we already visited this 'level'
+            if (level > list.Count())
+            {
+
+            }
+
+            // if we have a right node in a current subtree
+            if (node.right != null)
+            {
+                list.Add(node.val);
+                TraverseRight(node.right, list, level);
+            }
+            
+            if (node.left != null)
+        }
+    }
+}
